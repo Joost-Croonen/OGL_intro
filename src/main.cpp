daa6a8ee42cc255108c22cb0392356f575af8f6b
Joost Croonen HomePC <joost.croonen@live.be>
2025-07-03 12:50:47 +0200

    Hello triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -41,6 +55,95 @@ int main(void)
     // Handle resizing of viewport
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Compile and verify vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Compile and verify fragment shader
+    unsigned int fragementShader;
+    fragementShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragementShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragementShader);
+
+    glGetShaderiv(fragementShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragementShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Link and verify shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragementShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragementShader);
+
+    // Vertex data
+    float vertices[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+    };
+
+    // Vertex indices
+    unsigned int indices[] = {
+        0,1,3,
+        1,2,3
+    };
+
+    // Create vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // Create element buffer object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    // Create vertex attribute object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // Bind VAO
+    glBindVertexArray(VAO);
+    // Bind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Bind EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // Set vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Unbind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // Unbind VAO
+    glBindVertexArray(0);
+
+    // Enable wireframe ode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     // Main render loop
     while (!glfwWindowShouldClose(window)) 
     {
@@ -51,7 +154,12 @@ int main(void)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Core
+        // TRIANGLE
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Swap buffers and poll for IO events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
